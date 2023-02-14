3c9e1cf8581d8edf1910b09ca7e2b3ed0f8400da
Guangyuan Liu <gliu@dunnsolutions.com>
2023-02-14 11:45:50 -0600

    all about router and navigation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -11,9 +12,16 @@ export class RecipeListComponent implements OnInit {
   // @Output() RecipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
-  constructor(private RecipeService: RecipeService) {}
+  constructor(
+    private RecipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit() {
     this.recipes = this.RecipeService.getRecipes();
+  }
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
   // onRecipeSelected(recipe: Recipe) {
   //   this.RecipeWasSelected.emit(recipe);
