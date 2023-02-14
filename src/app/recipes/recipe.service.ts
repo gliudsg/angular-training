f40d2338346a473e0bf8e5ead2619b4e0be7cbf2
Guangyuan Liu <gliu@dunnsolutions.com>
2023-02-14 09:16:57 -0600

    add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,31 @@
-import { EventEmitter } from "@angular/core";
-import { Recipe } from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       'A test RCP',
       'this is TST',
-      'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg'
+      'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg',
+      [
+        new Ingredient('Apple', 5),
+        new Ingredient('banana', 6),
+      ]
     ),
     new Recipe(
       'test RCP 2222222222222',
       'this is TST',
-      'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg'
+      'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg',
+      [
+        new Ingredient('BEFF', 5),
+        new Ingredient('chincken', 6),
+      ]
     ),
   ];
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
 }
